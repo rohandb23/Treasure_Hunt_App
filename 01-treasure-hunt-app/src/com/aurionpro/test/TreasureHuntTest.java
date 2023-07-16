f3cc799dd24974e07946da4adca74e1d2f6b22bf
Rohan Bhise <Rohan.Bhise@asl.mum.sst>
2023-07-16 23:53:03 +0530

    v1

---
+++ 01-treasure-hunt-app/src/com/aurionpro/test/TreasureHuntTest.java
@@ -0,0 +1,29 @@
+package com.aurionpro.test;
+import java.util.Scanner;
+import com.aurionpro.model.TresureHunt;
+public class TreasureHuntTest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		TresureHunt obj = new TresureHunt();
+		System.out.println("welcome to treasure hunt game !!!!");
+		System.out.println("choose which direction to go : ");
+		String rightOrLeft = sc.nextLine();
+		boolean correctDirection = obj.chooseDirection(rightOrLeft);
+		if(correctDirection) {
+			System.out.println("Choose between swim or wait : ");
+			String swimOrWait = sc.nextLine();
+			boolean correctDecision = obj.chooseDecision(swimOrWait);
+			if(correctDecision) {
+				System.out.println("choose a door colour between red, blue and yellow : ");
+				String chooseDoorColour = sc.nextLine();
+				System.out.println(obj.chooseDoorColour(chooseDoorColour));
+			}
+			else {
+				System.out.println("attack bt trout GAME OVER!!!");
+			}
+		}
+		else {
+			System.out.println("fall into hole GAME OVER!!!");
+		}
+	}
+}
